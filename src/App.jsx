ba243f2a7a35277c5452012a35b375bb49b2b8db
Trynew19 <chauhankrishana42@gmail.com>
2024-05-27 15:35:51 +0530

    logo name

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ function App() {
     <Box minH="100vh" bg="grey" color="whitesmoke" px={6} py={8}>
       <CodeEditor />
       <Flex align="center" justify="center" margin="20px" flexDirection="column">
-      <h1>&copy; {2024} KRN CODE. All rights reserved.</h1>
-      <br />
       <h1>Made with &#10084; Krishna, Rishabh, Rahul, Nikita</h1>
+      <br />
+      <h1>&copy; @{2024} KRN CODE. All rights reserved.</h1>
+
     </Flex>
 
     </Box>
